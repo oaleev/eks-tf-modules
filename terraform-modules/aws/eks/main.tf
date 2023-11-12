51d2f855b2798f353e3d3a63b13c02d0c6bb51bb
mialeevs <mialeevs@pm.me>
2023-11-11 23:24:48 -0600

    Adding the eks module
    Signed-off-by: mialeevs <mialeevs@pm.me>

--- terraform-modules/aws/eks/main.tf
+++ terraform-modules/aws/eks/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0"
+      version = ">= 5.25.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
+      version = ">= 2.23.0"
     }
   }
 }
